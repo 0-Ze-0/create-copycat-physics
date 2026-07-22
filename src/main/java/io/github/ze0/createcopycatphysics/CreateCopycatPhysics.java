2e1028d1dbbd97ad5a4e23cbc7de783c989ee16c
O-Ze-0 <brennanmale@gmail.com>
2026-07-22 20:46:31 +0800

    Initial Create: Copycat Physics project setup
    - Renamed NeoForge template
    - Configured mod metadata
    - Established Gradle project structure
    - Prepared project for GitHub development

---
+++ src/main/java/io/github/ze0/createcopycatphysics/CreateCopycatPhysics.java
@@ -0,0 +1,13 @@
+package io.github.ze0.createcopycatphysics;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+
+@Mod(CreateCopycatPhysics.MOD_ID)
+public class CreateCopycatPhysics {
+
+    public static final String MOD_ID = "create_copycat_physics";
+
+    public CreateCopycatPhysics(IEventBus modEventBus) {
+    }
+}
---
+++ src/main/java/io/github/ze0/createcopycatphysics/CreateCopycatPhysicsClient.java
@@ -0,0 +1,15 @@
+package io.github.ze0.createcopycatphysics;
+
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+
+@Mod(
+        value = CreateCopycatPhysics.MOD_ID,
+        dist = Dist.CLIENT
+)
+public class CreateCopycatPhysicsClient {
+
+    public CreateCopycatPhysicsClient(IEventBus modEventBus) {
+    }
+}
